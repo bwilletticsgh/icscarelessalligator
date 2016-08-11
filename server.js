15f0a6d0918d85e2903175e0df9be495bc372e52
Tim Clark <tim.clark@inforeliance.com>
2016-08-11 13:08:31 -0400

    again

--- server.js
+++ server.js
@@ -1,11 +1,5 @@
 var express = require('express');
-var path = require('path');
 var app = express();
-
-app.use(express.static(path.join(__dirname, 'dist')));
-app.get('*', function(req, res) {
-  res.sendfile('./dist/index.html');
-});
-
+app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 3000);
 
