2ed9d317760e176a27105d4d59b8214e2159360c
bsuneson <bsuneson@exetergov.com>
2016-08-23 04:11:01 -0500

    Adding custom filter to enable CORS

---
+++ source/server/src/java/gov/dhs/kudos/rest/v1/filter/CorsFilter.java
@@ -0,0 +1,35 @@
+package gov.dhs.kudos.rest.v1.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author bsuneson
+ */
+public class CorsFilter implements Filter
+{
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException  {}
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException 
+    {
+        ((HttpServletResponse) response).setHeader("Access-Control-Allow-Origin", "*");
+        ((HttpServletResponse) response).setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        ((HttpServletResponse) response).setHeader("Access-Control-Expose-Headers", "Authorization");
+        ((HttpServletResponse) response).setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+        
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {}
+}
