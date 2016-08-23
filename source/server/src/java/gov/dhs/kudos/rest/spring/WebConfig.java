c1a90abd6eaf8e81d57908ea006ba5f8262d052a
bsuneson <bsuneson@exetergov.com>
2016-08-23 08:05:52 -0500

    Adding spring annotation configuration for swagger

---
+++ source/server/src/java/gov/dhs/kudos/rest/spring/WebConfig.java
@@ -0,0 +1,50 @@
+package gov.dhs.kudos.rest.spring;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ *
+ * @author bsuneson
+ */
+public class WebConfig extends WebMvcConfigurerAdapter
+{
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) 
+    {
+        converters.add(jackson2Converter());
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter jackson2Converter() 
+    {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(objectMapper());
+        return converter;
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() 
+    {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        return objectMapper;
+    }
+    
+     @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) 
+    {
+            registry.addResourceHandler("swagger-ui.html")
+                    .addResourceLocations("classpath:/META-INF/resources/");
+
+            registry.addResourceHandler("/webjars/**")
+                    .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+    }
+}
