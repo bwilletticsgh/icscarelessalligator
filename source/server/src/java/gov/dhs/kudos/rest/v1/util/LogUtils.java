036738adb10ea1057c352765bef60cb8b36ca570
bsuneson <bsuneson@exetergov.com>
2016-08-23 04:17:09 -0500

    Added method to filter an incoming URI for usage stats

--- source/server/src/java/gov/dhs/kudos/rest/v1/util/LogUtils.java
+++ source/server/src/java/gov/dhs/kudos/rest/v1/util/LogUtils.java
@@ -2,6 +2,8 @@ package gov.dhs.kudos.rest.v1.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Arrays;
+import java.util.List;
 import org.apache.log4j.Logger;
 
 /**
@@ -12,6 +14,8 @@ public class LogUtils
 {
     /** The logger for this class **/
     private static final Logger LOG = Logger.getLogger(LogUtils.class);
+    /** Holder of all endpoint URIs that contain parameters **/
+    private static final List<String> USAGE_PATH_FILTER;
     
     /**
      * Convert the object to a JSON string
@@ -32,5 +36,32 @@ public class LogUtils
             LOG.error(e);
             return "error converting obj to JSON";
         }
+    }
+
+    /**
+     * Filters the uri to not include and URI parameters - for logging UsageStats
+     * @param uri The uri to parse
+     * @return A URI without any parameters
+     */
+    public static String filterUsageURI(String uri) 
+    {
+        for(String pathFilter : USAGE_PATH_FILTER)
+        {
+            if(uri.startsWith(pathFilter))
+                return pathFilter;
+        }
+        return uri;
+    }
+    
+    static
+    {
+        USAGE_PATH_FILTER = Arrays.<String>asList(new String[] 
+        {
+            "/v1/user/byEmail","/v1/user/byId","/v1/user/byFirstName","/v1/user/byLastName",
+            "/v1/org/byName","/v1/org/create","/v1/org/addUser","/v1/org/cloneCat","/v1/org/createCat",
+            "/v1/cat/byName",
+            "/v1/kudos/fromUser/all","/v1/kudos/toUser/all","/v1/kudos/cat/all","/v1/kudos/create",
+            "/v1/usage/all","/v1/usage/byEmail","/v1/usage/byUri"
+        });
     }
 }
