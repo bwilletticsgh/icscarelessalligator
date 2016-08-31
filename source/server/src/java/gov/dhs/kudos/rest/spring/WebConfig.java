91c28daa7219e1b75b26fbb374e13727555c650c
bsuneson <bsuneson@exetergov.com>
2016-08-31 12:41:48 -0500

    Changed spring props load to annotation driven

--- source/server/src/java/gov/dhs/kudos/rest/spring/WebConfig.java
+++ source/server/src/java/gov/dhs/kudos/rest/spring/WebConfig.java
@@ -2,8 +2,13 @@ package gov.dhs.kudos.rest.spring;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import gov.dhs.kudos.rest.v1.notification.EmailNotification;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -13,8 +18,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * General purpose Spring MVC configuration
  * @author bsuneson
  */
+@Configuration
+@PropertySource("classpath:/gov/dhs/kudos/rest/spring/kudos.properties")
 public class WebConfig extends WebMvcConfigurerAdapter
 {
+    @Autowired
+    private Environment env;
+    
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) 
     {
@@ -35,6 +45,16 @@ public class WebConfig extends WebMvcConfigurerAdapter
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
         return objectMapper;
+    }
+    
+    @Bean
+    public EmailNotification emailNotification()
+    {
+        return new EmailNotification(env.getProperty("kudos.admin.email"), 
+                                     env.getProperty("smtp.host"), 
+                                     env.getProperty("smtp.port"), 
+                                     env.getProperty("smtp.user"), 
+                                     env.getProperty("smtp.auth"));
     }
     
      @Override
