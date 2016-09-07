dd4358ff6ba052cbacd42dfe0cb241463e5b8a4b
Tim Clark <tim.clark@inforeliance.com>
2016-09-07 15:39:48 -0400

    fixed unit tests

--- source/client/test/spec/controllers/registerCtrl.spec.js
+++ source/client/test/spec/controllers/registerCtrl.spec.js
@@ -2,6 +2,13 @@
 
 describe('Controller: RegisterCtrl', function () {
 
+  function resolve(val){
+    return $q(function(resolve){ resolve(val); });
+  }
+
+  function reject(val){
+    return $q(function(resolve, reject){ reject(val); });
+  }
   // load the controller's module
   beforeEach(module('kudosApp'));
 
@@ -9,16 +16,20 @@ describe('Controller: RegisterCtrl', function () {
     RegisterCtrl,
     $timeout,
     $httpBackend,
+    authentication,
+    $q,
     users,
     scope;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope, _$timeout_, _$httpBackend_, _users_) {
+  beforeEach(inject(function ($controller, $rootScope, _$timeout_, _$httpBackend_, _users_, _authentication_,_$q_) {
     $timeout=_$timeout_;
     users = _users_;
-    users.addUser({email:'someperson@somewhere.com', password:'abc'});
+    $q = _$q_;
+    authentication = _authentication_;
     $httpBackend=_$httpBackend_;
     $httpBackend.when('GET', 'views/main.html').respond(200);
+    $httpBackend.when('GET', 'views/account/login.html').respond(200);
     $httpBackend.when('GET', 'views/users/list.html').respond(200);
     scope = $rootScope.$new();
     RegisterCtrl = $controller('RegisterCtrl', {
@@ -29,29 +40,27 @@ describe('Controller: RegisterCtrl', function () {
 
   it('should allow a user to register', function () {
     RegisterCtrl.Username='theRandomPerson';
-
-    expect(users.getUser(RegisterCtrl.Username)).toBeUndefined();
-
     RegisterCtrl.Password='thepassword';
-    RegisterCtrl.Register().then(function(){
-      expect(RegisterCtrl.message).toBe(RegisterCtrl.successMessage);
-      expect(users.getUser(RegisterCtrl.Username)).toBeTruthy();
-    });
-    $httpBackend.flush();
-    $timeout.flush();
+    RegisterCtrl.firstName='firstname';
+    RegisterCtrl.lastName='lastname';
+    var stub = sinon.stub(authentication,'register').returns(resolve({success:true, message:'yay'}));
+    RegisterCtrl.Register();
+    scope.$digest();
+    expect(stub.calledWith(RegisterCtrl.Username, RegisterCtrl.Password, RegisterCtrl.firstName, RegisterCtrl.lastName)).toBe(true);
+    expect(RegisterCtrl.message).toBe(RegisterCtrl.successMessage);
   });
 
-  it('should not allow an email address to be reused', function () {
-    RegisterCtrl.Username='someperson@somewhere.com';
-
-    expect(users.getUser(RegisterCtrl.Username)).toBeTruthy();
-
+  it('should set the failure message to the message returned by the authentication service', function () {
+    RegisterCtrl.Username='theRandomPerson';
     RegisterCtrl.Password='thepassword';
-    RegisterCtrl.Register().then(function(){
-      expect(RegisterCtrl.failed).toBeTruthy();
-    });
-    $httpBackend.flush();
-    $timeout.flush();
+    RegisterCtrl.firstName='firstname';
+    RegisterCtrl.lastName='lastname';
+    var stub = sinon.stub(authentication,'register').returns(resolve({success:false, message:'no!'}));
+    RegisterCtrl.Register();
+    scope.$digest();
+    expect(stub.calledWith(RegisterCtrl.Username, RegisterCtrl.Password, RegisterCtrl.firstName, RegisterCtrl.lastName)).toBe(true);
+    expect(RegisterCtrl.message).toBe('no!');
   });
+
 
 });
