50f5fc92957eff973db3524bc787c90d0946522a
Tim Clark <tim.clark@inforeliance.com>
2016-09-09 02:39:31 -0400

    added spinner

--- source/client/app/scripts/app.js
+++ source/client/app/scripts/app.js
@@ -27,6 +27,17 @@ angular
     return $window.location.protocol + '//' + $window.location.hostname + KudosRESTAPIPortNumber + '/KudosREST/v1/';
   })
   .run(function ($rootScope, authentication, $state, $cookieStore, swal, _, users) {
+
+    $rootScope.stateIsLoading = false;
+    $rootScope.$on('$stateChangeStart', function() {
+      $rootScope.stateIsLoading = true;
+    });
+
+    $rootScope.$on('$stateChangeSuccess', function() {
+      $rootScope.stateIsLoading = false;
+    });
+
+
     _.each($state.get(), function(state){
       state.resolve = state.resolve || {};
     });
