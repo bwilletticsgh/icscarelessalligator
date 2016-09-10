f48ed2444baeb7021cb886094a66793528d57f2d
Michael Randers-Pehrson <michael.rp@gmail.com>
2016-09-10 16:49:20 -0400

    A few fixes to address 508 compliance errors in Wave. FormatTime changes to support lower case when needed.

--- source/client/app/scripts/filters/formatTime.js
+++ source/client/app/scripts/filters/formatTime.js
@@ -2,7 +2,11 @@
 angular.module('kudosApp')
   .constant('moment',window.moment)
   .filter('formatTime', function(moment) {
-    return function(input) {
-      return moment(parseInt(input)).calendar();
+    return function(input, lowerCase) {
+      var result = moment(parseInt(input)).calendar();
+      if(lowerCase) {
+        result = result.replace('Last', 'last').replace('Today', 'today').replace('Tomorrow', 'tomorrow').replace('Yesterday', 'yesterday');
+      }
+      return result;
     };
   });
